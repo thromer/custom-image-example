38cc42122997ad6d0fbf5f46c883f0edfed2b3d4
Ted Romer <tromer@gmail.com>
2025-07-02 10:38:58 -0700

    Tidy up scripts

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
+PROJECT=TODO
+
 cd $(realpath "$(dirname "${BASH_SOURCE[0]}")")/src &&
-    docker build -t us-west1-docker.pkg.dev/mlb-standings-001/artifacts/custom-image-example:latest . &&
-    docker push us-west1-docker.pkg.dev/mlb-standings-001/artifacts/custom-image-example:latest &&
+    docker build -t us-west1-docker.pkg.dev/${PROJECT}/artifacts/custom-image-example:latest . &&
+    docker push us-west1-docker.pkg.dev/${PROJECT}/artifacts/custom-image-example:latest &&
     gcloud run deploy \
-	   --image us-west1-docker.pkg.dev/mlb-standings-001/artifacts/custom-image-example \
+	   --image us-west1-docker.pkg.dev/${PROJECT}/artifacts/custom-image-example \
 	   --base-image us-west1-docker.pkg.dev/serverless-runtimes/google-22/runtimes/python312 \
 	   --region us-west1 custom-image-example \
 	   --concurrency 1 \
@@ -12,5 +14,5 @@ cd $(realpath "$(dirname "${BASH_SOURCE[0]}")")/src &&
 	   --timeout 1800 \
 	   --cpu=0.1 \
 	   --memory=256Mi &&
-    docker images ls -f 'reference=us-west1-docker.pkg.dev/mlb-standings-001/artifacts/custom-image-example*' | 
+    docker images ls -f 'reference=us-west1-docker.pkg.dev/${PROJECT}/artifacts/custom-image-example*' | 
 	tail -n +2 | awk '{print $3}' | xargs docker image rm
